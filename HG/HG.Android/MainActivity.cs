1bac98afaeae3fac5f1678e5729a2567e5ecd5e1
Will <Will@DESKTOP-FRBT6DQ>
2022-04-18 21:28:17 -0400

    Agregar archivos de proyecto.

---
+++ HG/HG.Android/MainActivity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Android.Views;
+
+namespace HG.Droid
+{
+    [Activity(Label = "HG", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            //ocultar status bar
+
+            Window.AddFlags(WindowManagerFlags.Fullscreen);
+            Window.ClearFlags(WindowManagerFlags.ForceNotFullscreen);
+
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+
+        
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public bool OnTouch(Android.Views.View v, MotionEvent e)
+        {
+            if (e.Action == MotionEventActions.Down)
+            {
+                v.SoundEffectsEnabled = true;
+                v.PlaySoundEffect(SoundEffects.Click);
+            }
+            return true;
+        }
+
+
+    }
+}
